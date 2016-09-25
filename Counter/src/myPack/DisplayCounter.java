f5a60d834cabceec4694654f2db2f99daec616bc
hyhoang <bluegasus@gmail.com>
2016-09-25 21:20:10 +0700

    hello

---
+++ Counter/src/myPack/DisplayCounter.java
@@ -0,0 +1,56 @@
+package myPack;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DisplayCounter
+ */
+@WebServlet("/DisplayCounter")
+public class DisplayCounter extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DisplayCounter() {
+       
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		super.init(config);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		ServletContext servletContext = getServletContext();
+		
+		Integer counter = (Integer) servletContext.getAttribute("Counter");
+		
+		PrintWriter out = response.getWriter();
+		
+		response.setContentType("text/html");
+		out.println("<html><head><title>Display Counter</title></head><body>");
+		out.println("<p>The counter value is currently:" + counter + "</p>");
+		out.println("</body></html>");
+		
+	}
+
+}
